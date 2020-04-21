1775fca32c6e1b208de1a7759118cd566fa81477
Grant Sander <gksander93@gmail.com>
2020-04-20 22:30:59 -0500

    4xl max w

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -9,7 +9,7 @@ const SiteLayout: React.FC = ({ children }) => {
     <div style={{ fontSize: "18px" }}>
       <Sidebar />
       <div className="relative md:ml-64 bg-gray-100 px-4 md:px-10 py-4 min-h-screen">
-        <div className="max-w-3xl mx-auto">{children}</div>
+        <div className="max-w-4xl mx-auto">{children}</div>
       </div>
     </div>
   );
